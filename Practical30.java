04bdbcab29bcfaa8945f0be931217d0bd89de7e7
Dhruvi Patel <dhruvipatel2045@gmail.com>
2026-04-24 22:21:58 +0530

    Update Practical30.java

--- Practical30.java
+++ Practical30.java
@@ -1,53 +1,46 @@
-abstract class Vehicle
-{
-    abstract String fuelType();
-    abstract int noOfWheels();
+/**Write a Java program to create an abstract class Vehicle with:
+An abstract method fuelType() that returns the type of fuel used.
+An abstract method noOfWheels() that returns the number of wheels.
+Create two subclasses:
+Car that uses Petrol/Diesel and has 4 wheels.
+Bike that uses Petrol and has 2 wheels.
+In the main method, create objects of Car and Bike, and display their fuel type and number
+Of wheels.*/
+
+abstract class Vehicle {
+	abstract String fuelType();
+	abstract int noOfWheels();
 }
 
-class Car extends Vehicle
-{
-    String fuelType()
-    {
-        return "Petrol/Diesel";
-    }
-
-    int noOfWheels()
-    {
-        return 4;
-    }
+class Car extends Vehicle {
+	String fuelType() {
+		return "Petrol/Diesel";
+	}
+	int noOfWheels() {
+ 		return 4;
+ 	}
 }
 
-class Bike extends Vehicle
-{
-    String fuelType()
-    {
-        return "Petrol";
-    }
-
-    int noOfWheels()
-    {
-        return 2;
-    }
+class Bike extends Vehicle {
+	String fuelType() {
+ 		return "Petrol";
+ 	}
+ 	int noOfWheels() {
+ 		return 2;
+ 	}
 }
 
-class MainClass
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Name: Dhruvi Patel");
-        System.out.println("Enrollment No: 240390107027");
-        System.out.println();
-
-        Vehicle v1 = new Car();
-        System.out.println("Car Details:");
-        System.out.println("Fuel Type: " + v1.fuelType());
-        System.out.println("Wheels: " + v1.noOfWheels());
-
-        System.out.println();
-
-        Vehicle v2 = new Bike();
-        System.out.println("Bike Details:");
-        System.out.println("Fuel Type: " + v2.fuelType());
-        System.out.println("Wheels: " + v2.noOfWheels());
-    }
+class Fueltype {
+	public static void main(String[] args) {
+		System.out.println("Dhruvi Patel");
+		System.out.println("240390107027");
+		Vehicle v1 = new Car();
+		Vehicle v2 = new Bike();
+		System.out.println("\nCar Details:");
+		System.out.println("Fuel Type: " + v1.fuelType());
+		System.out.println("Number of Wheels: " + v1.noOfWheels());
+		System.out.println("\nBike Details:");
+		System.out.println("Fuel Type: " + v2.fuelType());
+		System.out.println("Number of Wheels: " + v2.noOfWheels());
+ 	}
 }
