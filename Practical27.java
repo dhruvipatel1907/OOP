54e71c2ca0a89cef450941cdffac99638dfa20d7
Dhruvi Patel <dhruvipatel2045@gmail.com>
2026-04-24 21:39:43 +0530

    Refactor Result class and update main method

--- Practical27.java
+++ Practical27.java
@@ -1,41 +1,25 @@
-import java.util.Scanner;
+/**Given an interface Classify with a method String getDivision(double average). Implement this getDivision method in a class Result such that it returns “First Division” if the average is 60 or more.*/
 
-interface Classify
-{
-    String getDivision(double average);
+interface Classify {
+	String getDivision(double average);
 }
 
-class Result implements Classify
-{
-    public String getDivision(double average)
-    {
-        if (average >= 60)
-        {
-            return "First Division";
-        }
-        else
-        {
-            return "No First Division";
-        }
-    }
+class Result implements Classify {
+	@Override
+	public String getDivision(double average) {
+		if (average >= 60) {
+			return "First Division";
+		} else {
+			return "Below First Division";
+		}
+ 	}
 }
 
-class MainClass
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Name: Dhruvi Patel");
-        System.out.println("Enrollment No: 240390107027");
-        System.out.println();
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter average marks: ");
-        double avg = sc.nextDouble();
-
-        Result r = new Result();
-        String division = r.getDivision(avg);
-
-        System.out.println("Division: " + division);
-    }
+class Result1 {
+	public static void main(String[] args) {
+		System.out.println("Dhruvi Patel");
+		System.out.println("240390107027");
+		Result r = new Result();
+		System.out.println(r.getDivision(75)); 
+	}
 }
